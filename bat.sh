5d58d68d5ff4378e20730a22c68223760738d8f1
prayletter <miaohui821@hotmail.com>
2016-06-19 20:17:18 +0800

    update bat.sh

--- bat.sh
+++ bat.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#$1 命令行第一个参数
+#$1 命令行第一个参数 $2 命令行第二个参数
+#运行格式 ./bat.sh leetcode 题目编号 leetcode题目名称
 cat  /Users/miaohui/ClionProjects/leetcode/main.cpp \
 			 	>> /Users/miaohui/Documents/LeetCode/Medium/$1.cpp
 git add  Medium/$1.cpp
