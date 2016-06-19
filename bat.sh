6181096fe16d075ba85bd96e25345b7a3568fc79
prayletter <miaohui821@hotmail.com>
2016-06-19 20:13:46 +0800

    update bat.sh

--- bat.sh
+++ bat.sh
@@ -2,5 +2,6 @@
 #$1 命令行第一个参数
 cat  /Users/miaohui/ClionProjects/leetcode/main.cpp \
 			 	>> /Users/miaohui/Documents/LeetCode/Medium/$1.cpp
-git commit
-git push
+git add  Medium/$1.cpp
+git commit -m "$2"
+git push -u origin master
