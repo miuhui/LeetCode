5b35a96e456cffc625a4182d6f00b40149edb93e
prayletter <miaohui821@hotmail.com>
2016-07-13 17:58:15 +0800

    Add SH For Windows

---
+++ bat_windows.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#$1 命令行第一个参数 $2 命令行第二个参数
+#运行格式 ./bat.sh leetcode 题目编号 leetcode题目名称
+cat  C:/Users/MiaoHui/OneDrive/Projects/leetcode/leetcode/leetcode.cpp \
+			 	>> C:/Users/MiaoHui/Documents/GitHub/LeetCode/Medium/$1.cpp
+git add  Medium/$1.cpp
+git commit -m "$2"
+git push -u origin master
