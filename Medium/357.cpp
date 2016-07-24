df062739ecf36c90a1b0bd948683408c050a4fdd
prayletter <miaohui821@hotmail.com>
2016-07-24 18:08:48 +0800

    CountNumbersWithUniqueDigits

--- Medium/357.cpp
+++ Medium/357.cpp
@@ -66,3 +66,37 @@ int main()
 	getchar();
 	return 0;
 }
+// leetcode.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:	
+	static int countNumbersWithUniqueDigits(int n) {
+		if (n == 0) return 1;
+		// when n = 1, return 10
+		auto result = 10;
+		// f(k) = 10 + 9 * 8 * ... * (11 - k)
+		auto t = 9;
+		for (auto i = 2; i <= n; i++)
+		{
+			t *= 11 - i;
+			result += t;
+		}
+		return result;
+	}
+
+};
+
+int main()
+{	
+	Solution s;
+	cout << s.countNumbersWithUniqueDigits(3) << endl;
+	getchar();
+	return 0;
+}
